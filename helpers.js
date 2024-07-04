b5926c8638791a8ca0124b0e910d2782daadc5b1
Shadman Rahman <shadmxn@gmail.com>
2024-07-04 10:45:33 -0400

    implement mentor feedback from previous submission with the help of Larry Ai as well as implementing more comments throughout the code

--- helpers.js
+++ helpers.js
@@ -1,3 +1,5 @@
+// Function to retrieve a user object by their email from a given database
+
 const getUserByEmail = function(email, database) {
   for (const userId in database) {
     const user = database[userId];
